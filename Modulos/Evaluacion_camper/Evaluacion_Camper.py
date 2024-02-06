9cee7cd8a0045eb37e12e53e3445a00baac7b7ca
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-02-06 07:15:07 -0500

    feat: :construction: proyecto Python progreso
    En este progreso se modifico el modulo de Evaluacion_Camper, el cual le permite al coordinador asignarle al camper las pruebas teoricas y practicas, para respectivamente asignarle el nuevo estado al camper.

--- Modulos/Evaluacion_camper/Evaluacion_Camper.py
+++ Modulos/Evaluacion_camper/Evaluacion_Camper.py
@@ -5,7 +5,7 @@ from os import system
 # IMPORTACION DE LOS MODULOS PARA PODER INGRESAR #
 from Modulos.Validaciones.Validaciones import menuNoValid
 from Storage.Datos.datos import camper
-
+from Storage.Datos.datos import Estados
 
 
 
@@ -38,20 +38,36 @@ ________________________
         print("¿Este es el camper al que desea asignar nota? \n")
         print("1. Si")
         print("2. No")
-        print("3. Salir")
+        print("3. Salir\n")
         opc = int(input("Seleccione su opcion \n"))
         if (opc == 1):
-            print ("aqui voy")
+            Nota_teorica = int(input("ingrese el valor de la nota teorica \n"))
+            Nota_Practica = int(input("ingrese el valor de la nota practica \n"))
+            Promedio = (Nota_teorica + Nota_Practica) / 2
+            if Promedio <= 59:
+                return  print("! LO SENTIMOS EL CAMPERO NO HA SUPERADO LA PRUEBA! \n")
+            else:
+                print("! EL CAMPER HA SUPERADO LA PRUEBA !")
+                info = {
+                    "Estado": int(input("Asigne el Estado del camper:\n\t"+"\t".join([f"{Estados.index(i)+1}. {i}\n" for i in (Estados)])))            
+                }
+                camper.append(info)
+                with open("Storage/Campers/camper.json", "w") as f: 
+                 datos = json.dumps(camper, indent=4)
+                 f.write(datos)
+                 f.close()
+                 return print("! CAMPER ASCENDIDO HA INSCRITO")            
         elif (opc == 2):
             bandera = False
         elif (opc == 3):
             system("clear")
             bandera = False
+
             
 def menu():
     bandera = True
     while (bandera):
-        print(""" *******************************
+        print("""*******************************
 MODULO DE EVALUACION DEL CAMPER
 *******************************
 CRUD del modulo de Evaluacion del camper
